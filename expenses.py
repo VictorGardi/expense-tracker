b27a8e5ac8e831d068e1f303483ab61429ac0d33
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-10-27 20:15:39 +0200

    add categories file

--- expenses.py
+++ expenses.py
@@ -1,4 +1,6 @@
+import json
 import os
+from typing import Dict, List
 
 import numpy as np
 import pandas as pd
@@ -9,6 +11,21 @@ s = Splitwise(
     consumer_secret=os.environ["CONSUMER_SECRET"],
     api_key=os.environ["API_KEY"],
 )
+
+
+def add_category(df: pd.DataFrame, words: List, category_name: str) -> pd.DataFrame:
+    df["category"] = np.where(
+        sum(df.description_lowercase.str.contains(word) for word in words),
+        category_name,
+        df.category,
+    )
+    return df
+
+
+def read_json(path_to_file: str) -> Dict:
+    with open(path_to_file) as f:
+        data = json.load(f)
+    return data
 
 
 def get_expenses(start_date, end_date) -> pd.DataFrame:
@@ -23,16 +40,10 @@ def get_expenses(start_date, end_date) -> pd.DataFrame:
     df["description"] = expense_descriptions
     df["cost"] = expense_costs
     df["date"] = expense_dates
-    food_words = ["hemköp", "willys", "mat", "coop", "ica"]
     df["description_lowercase"] = df.description.str.lower()
-    df["category"] = np.where(
-        sum(df.description_lowercase.str.contains(food_word) for food_word in food_words),
-        "food",
-        "other",
-    )
-    df["category"] = np.where(
-        df.description_lowercase.str.contains("restaurang"), "eat_out", df.category
-    )
+    df["category"] = "other"
+    for category, words in read_json("categories.json").items():
+        df = add_category(df, words, category)
     df["date"] = pd.to_datetime(df.date, format="%Y-%m-%dT%H:%M:%SZ")
     df["year-month"] = df.date.dt.strftime("%Y-%m")
     return df
