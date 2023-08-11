af60c41ce6a28a9803ba6c1ffd4d000ffc5ddcd5
Victor Olsson <victor.olssn@gmail.com>
2023-08-11 10:28:16 +0200

    add python package config

--- src/expense_tracker/utils.py
+++ src/expense_tracker/utils.py
@@ -2,4 +2,4 @@ import streamlit_authenticator as stauth
 
 
 def generate_hashed_pw(unhashed_pw: str) -> str:
-    return stauth.Hasher([unhashed_pw]).generate()
+    return stauth.Hasher([unhashed_pw]).generate()[0]
